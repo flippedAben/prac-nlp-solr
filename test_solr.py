f0e7752a85ef29e30097d6af19fa1b8fddc34aff
angelocioffi <angelo.cioffi@utexas.edu>
2019-02-20 03:52:00 -0600

    Updated testing script to test precision, recall and rank.

--- test_solr.py
+++ test_solr.py
@@ -1,31 +1,59 @@
 import requests
+import json
 import decimal
 
 ENDPOINT = "http://localhost:9000/solr/movies/query"
 
 r_headers = {"Content-Type": "application/json"}
 
-correct_movie = "Space Jam"
+with open('./goldStandardWithSeries.json') as raw_turk_data:
+	turk_data = json.load(raw_turk_data)
 
-query_params = {'df':'title','q':'Space Jam'}
+precision_total_score = 0
+recall_total_score = 0
+total_rank = 0
+total_row_count = 0
+total_correct_response_count = 0;
 
-query_response = requests.get(ENDPOINT, params=query_params, headers=r_headers)
 
-print(query_response.status_code)
 
-response_parsed = (''.join(query_response.json()["response"]["docs"][0]["title"])).replace("'", "-").split("-")[0]
+for movie_row in turk_data:
+	movie_description = movie_row[0]
+	correct_movie = movie_row[2]
+	query_params = {'defType':'edismax', 'qf':'cast.name cast.character directors.name genres original_title overview production_companies production_countries release_date spoken_languages tagline',
+	                'q':movie_description}
+	query_response = requests.get(ENDPOINT, params=query_params, headers=r_headers)
+	#print(query_response.status_code)
+	correct_response_counter = 0
+	rank = 0
+	for i in query_response.json()["response"]["docs"]:
+		rank += 1
+		movie_title = (''.join(i["title"])).replace("'", "-").split("-")[0]
+		if movie_title == correct_movie:
+			correct_response_counter += 1
+			total_rank += rank
 
-print(response_parsed)
+	total_movies = query_response.json()["response"]["numFound"]
 
-correct_response_counter = 0
+	if total_movies > 0:
+		precision = decimal.Decimal(correct_response_counter)/decimal.Decimal(total_movies)
+		recall = 1
+	else:
+		recall = 0
 
-for i in query_response.json()["response"]["docs"]:
-	movie_title = (''.join(i["title"])).replace("'", "-").split("-")[0]
-	if movie_title == correct_movie:
-		correct_response_counter += 1
+	precision_total_score += precision
+	recall_total_score += recall
+	total_row_count += 1
 
-total_movies = query_response.json()["response"]["numFound"]
+	total_correct_response_count += correct_response_counter
 
-precision = decimal.Decimal(correct_response_counter)/decimal.Decimal(total_movies)
+	print(total_row_count)
 
-print(precision)
+
+precision_total_avg = decimal.Decimal(precision_total_score) / decimal.Decimal(total_row_count)
+recall_total_avg = decimal.Decimal(recall_total_score) / decimal.Decimal(total_row_count)
+rank_total_avg = decimal.Decimal(total_rank) / decimal.Decimal(total_correct_response_count)
+
+print "Average Precision:", str(precision_total_avg)
+print "Average Recall:", str(recall_total_avg)
+print "Average Rank:", str(rank_total_avg)
